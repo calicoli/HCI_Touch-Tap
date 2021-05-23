469c0127e8806a0acb5fc2d8c45ffd26e982ccf9
Calico <ireneecs@qq.com>
2021-05-23 23:57:15 +0800

    preliminary demo
    include server and client

---
+++ Client-touch/Assets/Scripts/Lab1-tap/TargetScheduler.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetScheduler : MonoBehaviour
+{
+
+    public GameObject targets2;
+    //public GameObject startBtn;
+
+    const int repeatTime = 1;
+    const int columnCnt= 5, rowCnt = 5;
+    const int target2Cnt = 25;
+
+    struct Trial
+    {
+        public int index;
+        public int secondid;
+
+        public void setParams(int idx, int id2)
+        {
+            index = idx;
+            secondid = id2;
+        }
+
+        public void printParams()
+        {
+            Debug.Log("no." + index + " id2: " + secondid);
+        }
+    }
+
+    enum TargetStatus
+    {
+        NORMAL, CORRECT, WRONG,
+    }
+    struct Target
+    {
+        public int id;
+        public int remainTouchCnt;
+        public bool visible;
+        public TargetStatus status;
+
+        public Target(int idx, int cnt, bool vis, TargetStatus st)
+        {
+            id = idx;
+            remainTouchCnt = cnt;
+            visible = vis;
+            status = st;
+        }
+    }
+
+    int curTrialIndex;
+    Trial[] trials = new Trial[100 + 1];     // trial[0] is empty
+    int totalCubes2;
+    Target[] cubes2 = new Target[50];
+    ArrayList arrayRemainTargets2 = new ArrayList();
+    //Vector3[] posCubes2 = new Vector3[50];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        curTrialIndex = 1;
+
+        // target2 set
+        arrayRemainTargets2.Clear();
+        totalCubes2 = target2Cnt;
+        for (int i = 0; i < totalCubes2; i++)
+        {
+            GameObject child = targets2.transform.GetChild(i).gameObject;
+            if (child.name.Length == 11)
+            {
+                int id2 = Convert.ToInt32(child.name.Substring(9, 2));
+                cubes2[id2] = new Target(id2, repeatTime, false, TargetStatus.NORMAL);
+                arrayRemainTargets2.Add(id2);
+                //posCubes2[id2] = child.transform.position;
+            }
+        }
+
+        Debug.Log("cnt set 2: " + arrayRemainTargets2.Count);
+        resetAllCubes2();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void resetAllCubes2()
+    {
+        for (int id2 = 0; id2 < totalCubes2; id2++)
+        {
+            cubes2[id2].visible = false;
+            cubes2[id2].status = TargetStatus.NORMAL;
+            targets2.transform.GetChild(id2).gameObject.SetActive(cubes2[id2].visible);
+        }
+    }
+
+    public void scheduleTargetsWithServerData(int sTrialIndex, int sTarget2id)
+    {
+        curTrialIndex = sTrialIndex;
+        updateTarget2Array(sTarget2id);
+        trials[curTrialIndex].setParams(sTrialIndex, sTarget2id);
+        trials[curTrialIndex].printParams();
+    }
+
+    void updateTarget2Array(int id2)
+    {
+        cubes2[id2].remainTouchCnt--;
+        if (cubes2[id2].remainTouchCnt <= 0)
+        {
+            arrayRemainTargets2.Remove(id2);
+        }
+        Debug.Log("arrayCount2/id/cntTouch: " 
+            + arrayRemainTargets2.Count + " / " 
+            + id2 + " / " 
+            + cubes2[id2].remainTouchCnt);
+    }
+
+    public void updateTarget2(bool isActive)
+    {
+        int secondid = trials[curTrialIndex].secondid;
+        cubes2[secondid].visible = isActive;
+        targets2.transform.GetChild(secondid).gameObject.SetActive(cubes2[secondid].visible);
+    }
+
+    public int getCurrentTarget2id()
+    {
+        return trials[curTrialIndex].secondid;
+    }
+}
---
+++ Server-touch/Assets/Scripts/Lab1-tap/ServerController.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ServerController : MonoBehaviour
+{
+    public Text ipText;
+    public Camera renderCamera;
+
+    public GameObject angleProcessor;
+    public GameObject trialController;
+
+    private Color disconnectColor = new Color(0.8156f, 0.3529f, 0.4313f);
+    private Color connectColor = new Color(0f, 0f, 0f);
+
+    private Vector3 pos = new Vector3(0, 0, 0);
+
+    private TcpListener tcpListener;
+    private Thread tcpListenerThread;
+    private TcpClient connectedTcpClient;
+    private string receiveMsg = "";
+    private bool clientRefreshed = false;
+
+    private bool noConnection = true;
+
+    void Start()
+    {
+        tcpListenerThread = new Thread(new ThreadStart(ListenForIncommingRequests));
+        tcpListenerThread.IsBackground = true;
+        tcpListenerThread.Start();
+    }
+
+    void Update()
+    {
+        ipText.text = getIPAddress();
+        bool isConnecting = connectedTcpClient != null;
+        renderCamera.backgroundColor = (isConnecting ? connectColor : disconnectColor);
+        angleProcessor.GetComponent<AngleProcessor>().isConnecting = isConnecting;
+        trialController.GetComponent<TrialController>().isConnecting = isConnecting;
+
+        if (isConnecting && noConnection)
+        {
+            // the first time to sendMessage();
+            sendMessage(true, true);
+            noConnection = false;
+        }
+        if (clientRefreshed)
+        {
+            getVector();
+            clientRefreshed = false;
+        }
+    }
+
+    private void ListenForIncommingRequests()
+    {
+        try
+        {
+            tcpListener = new TcpListener(IPAddress.Any, 8052);
+            tcpListener.Start();
+            Debug.Log("Server is listening");
+            Byte[] bytes = new Byte[1024];
+            while (true)
+            {
+                using (connectedTcpClient = tcpListener.AcceptTcpClient())
+                {
+                    using (NetworkStream stream = connectedTcpClient.GetStream())
+                    {
+                        int length;
+                        while ((length = stream.Read(bytes, 0, bytes.Length)) != 0)
+                        {
+                            var incommingData = new byte[length];
+                            Array.Copy(bytes, 0, incommingData, 0, length);
+                            receiveMsg = Encoding.ASCII.GetString(incommingData);
+                            clientRefreshed = true;
+                        }
+                    }
+                }
+            }
+        }
+        catch (SocketException socketException)
+        {
+            Debug.Log("SocketException " + socketException.ToString());
+        }
+    }
+
+    private void sendMessage(bool sendAngleData, bool sendTrialData)
+    {
+        if (connectedTcpClient == null)
+        {
+            return;
+        }
+        string flag;
+        flag = sendAngleData ? "T" : "F";
+        flag = sendTrialData ? flag + "T" : flag + "F";
+        float angle = angleProcessor.GetComponent<AngleProcessor>().getAngle();
+        string sLabName;
+        int sTrialIndex, sTrialPhase, sTarget2id;
+        trialController.GetComponent<TrialController>().
+            getParams4Client(out sLabName, out sTrialIndex, out sTrialPhase, out sTarget2id);
+
+        try
+        {
+            NetworkStream stream = connectedTcpClient.GetStream();
+            if (stream.CanWrite)
+            {
+                string serverMessage =
+                    flag + "," +
+                    angle + "," +
+                    sLabName + "," +
+                    sTrialIndex + "," +
+                    sTrialPhase + "," +
+                    sTarget2id + ","
+                    ;
+                byte[] serverMessageAsByteArray = Encoding.ASCII.GetBytes(serverMessage);
+                stream.Write(serverMessageAsByteArray, 0, serverMessageAsByteArray.Length);
+                Debug.Log("Server sent his message - should be received by client");
+                Debug.Log("S sendMsg: " + serverMessage);
+            }
+        }
+        catch (SocketException socketException)
+        {
+            Debug.Log("Socket exception: " + socketException);
+        }
+    }
+
+    private string getIPAddress()
+    {
+        var host = Dns.GetHostEntry(Dns.GetHostName());
+        foreach (var ip in host.AddressList)
+        {
+            if (ip.AddressFamily == AddressFamily.InterNetwork)
+            {
+                return ip.ToString();
+            }
+        }
+        throw new System.Exception("No network adapters with an IPv4 address in the system!");
+    }
+
+    private void getVector()
+    {
+        Debug.Log("S rcvMsg: " + receiveMsg);
+        string[] messages = receiveMsg.Split(',');
+
+        char refreshAngle = messages[0][0];
+        char refreshTrial = messages[0][1];
+
+        if (refreshAngle - 'T' == 0)
+        {
+            Vector3 accClient = new Vector3(
+                System.Convert.ToSingle(messages[1]),
+                System.Convert.ToSingle(messages[2]),
+                System.Convert.ToSingle(messages[3]));
+        }
+        if (refreshTrial - 'T' == 0)
+        {
+            string cLabName = messages[4];
+            int cTrialIndex = System.Convert.ToInt32(messages[5]);
+            int cTrialPhase = System.Convert.ToInt32(messages[6]);
+            int cTarget2id = System.Convert.ToInt32(messages[7]);
+            bool cPhaseFinished = (messages[8][0] - 'T' == 0);
+            bool cPhaseSuccess = (messages[8][1] - 'T' == 0);
+
+            bool sendMessageToClientAgain = false;
+            sendMessageToClientAgain = trialController.GetComponent<TrialController>().
+                checkClientTargetTouch(cLabName, cTrialIndex, cTrialPhase, cTarget2id, cPhaseFinished, cPhaseSuccess);
+            if (!sendMessageToClientAgain)
+            {
+                prepareNewMessage4Client(false, true);
+            }
+
+        }
+    }
+    public void prepareNewMessage4Client(bool refreshAngle, bool refreshTrial)
+    {
+        sendMessage(refreshAngle, refreshTrial);
+    }
+}
